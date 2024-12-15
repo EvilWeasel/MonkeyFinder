debb15455792360ab9c2ac8d74d7a8317c88eb4f
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2024-12-15 19:08:55 +0100

    GetMonkeysCommand base implementation using maui icommand

---
+++ Services/MonkeyService.cs
@@ -0,0 +1,34 @@
+﻿using MonkeyFinder.Model;
+using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+public class MonkeyService
+{
+  public const string URL = "https://montemagno.com/monkeys.json";
+  HttpClient client;
+  public MonkeyService()
+  {
+    client = new();
+  }
+  List<Monkey> monkeyList = [];
+  public async Task<List<Monkey>> GetMonkeys()
+  {
+    if (monkeyList.Count > 0)
+    {
+      return monkeyList;
+    }
+
+    var response = await client.GetAsync(URL);
+
+    if (response.IsSuccessStatusCode)
+    {
+      var monkeys = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+      if (monkeys != null)
+      {
+        monkeyList = monkeys;
+      }
+    }
+
+    return monkeyList;
+  }
+}
--- ViewModel/MonkeyViewModel.cs
+++ ViewModel/MonkeyViewModel.cs
@@ -1,5 +1,47 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Model;
+using MonkeyFinder.Services;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace MonkeyFinder.ViewModel;
 public partial class MonkeyViewModel : BaseViewModel
 {
+  private MonkeyService monkeyService = new();
+  public ObservableCollection<Monkey> Monkeys { get; set; } = [];
 
+  public Command GetMonkeysCommand { get; }
+
+  public MonkeyViewModel(MonkeyService monkeyService)
+  {
+    Title = "Monkey Finder";
+    this.monkeyService = monkeyService;
+    GetMonkeysCommand = new Command(
+      async () => await GetMonkeysAsync());
+  }
+
+  private async Task GetMonkeysAsync()
+  {
+    if (IsBusy)
+      return;
+    try
+    {
+      IsBusy = true;
+      var monkeys = await monkeyService.GetMonkeys();
+
+      if (monkeys.Count != 0)
+        Monkeys.Clear();
+
+      foreach (var monkey in monkeys)
+        Monkeys.Add(monkey);
+    }
+    catch (Exception ex)
+    {
+      Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
+      await Shell.Current.DisplayAlert("Error", $"Unable to get monkeys: {ex.Message}", "OK");
+    }
+    finally
+    {
+      IsBusy = false;
+    }
+  }
 }
