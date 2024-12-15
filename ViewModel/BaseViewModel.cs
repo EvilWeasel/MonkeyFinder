5395b7a37d4e4ebfa7a8943505e3c1de1b857d32
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2024-12-15 18:41:32 +0100

    implemented mvvm using community toolkit, baseviewmodel ready

--- ViewModel/BaseViewModel.cs
+++ ViewModel/BaseViewModel.cs
@@ -1,67 +1,19 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace MonkeyFinder.ViewModel;
-public class BaseViewModel : INotifyPropertyChanged
-{
-  bool isBusy;
-  //public bool IsBusy
-  //{
-  //  get => isBusy;
-  //  set
-  //  {
-  //    if(isBusy == value)
-  //      return;
-  //    isBusy = value;
-  //    // Notify the UI that the value has changed
-  //    // compiler inserts the name of the property, but is still a string
-  //    OnPropertyChanged(nameof(IsBusy));
-  //  }
-  //}
-  public bool IsBusy
-  {
-    get => isBusy;
-    set
-    {
-      if (isBusy == value)
-        return;
-      isBusy = value;
-      // Notify the UI that the value has changed
-      // compiler inserts the name of the property, but is still a string
-      OnPropertyChanged();
-      OnPropertyChanged(nameof(IsNotBusy));
-    }
-  }
 
+public partial class BaseViewModel : ObservableObject
+{
+  public BaseViewModel()
+  {
+    
+  }
+  [ObservableProperty]
+  [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+  bool isBusy;
+
+  [ObservableProperty]
+  string title;
   public bool IsNotBusy => !IsBusy;
 
-  string title;
-  public string Title
-  {
-    get => title;
-    set
-    {
-      if (title == value)
-        return;
-      title = value;
-      OnPropertyChanged();
-    }
-  }
-
-  public event PropertyChangedEventHandler? PropertyChanged;
-
-  /* this version uses string to get the member name which changed */
-  //public void OnPropertyChanged(string name)
-  //{
-  //  PropertyChanged?.Invoke(
-  //    this,
-  //    new PropertyChangedEventArgs(name));
-
-  //}
-  public void OnPropertyChanged([CallerMemberName] string? name = null)
-  {
-    PropertyChanged?.Invoke(
-      this,
-      new PropertyChangedEventArgs(name));
-  }
 }
